a1fdc41e3eb679e3016c0c1cedcb606c13bbc1d8
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-04-22 11:09:07 -0500

    feat: adding practice with TextBox Matriz Array

---
+++ WFmatArregloTextbox/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WFmatArregloTextbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBoxM = new System.Windows.Forms.TextBox();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.buttonSize = new System.Windows.Forms.Button();
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.listBoxMatrix = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.firmaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(78, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(185, 174);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBoxM
+            // 
+            this.textBoxM.Location = new System.Drawing.Point(78, 327);
+            this.textBoxM.Name = "textBoxM";
+            this.textBoxM.Size = new System.Drawing.Size(78, 20);
+            this.textBoxM.TabIndex = 1;
+            this.textBoxM.Text = "0";
+            this.textBoxM.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(185, 327);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(78, 20);
+            this.textBoxN.TabIndex = 2;
+            this.textBoxN.Text = "0";
+            this.textBoxN.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // buttonSize
+            // 
+            this.buttonSize.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.buttonSize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSize.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonSize.Location = new System.Drawing.Point(133, 364);
+            this.buttonSize.Name = "buttonSize";
+            this.buttonSize.Size = new System.Drawing.Size(75, 23);
+            this.buttonSize.TabIndex = 3;
+            this.buttonSize.Text = "Tamaño";
+            this.buttonSize.UseVisualStyleBackColor = false;
+            this.buttonSize.Click += new System.EventHandler(this.buttonSize_Click);
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.buttonRead.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRead.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonRead.Location = new System.Drawing.Point(300, 163);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(93, 23);
+            this.buttonRead.TabIndex = 4;
+            this.buttonRead.Text = "Leer Matriz";
+            this.buttonRead.UseVisualStyleBackColor = false;
+            this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.buttonLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLoad.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonLoad.Location = new System.Drawing.Point(300, 204);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(93, 23);
+            this.buttonLoad.TabIndex = 5;
+            this.buttonLoad.Text = "Poner Matriz";
+            this.buttonLoad.UseVisualStyleBackColor = false;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // listBoxMatrix
+            // 
+            this.listBoxMatrix.FormattingEnabled = true;
+            this.listBoxMatrix.Items.AddRange(new object[] {
+            " "});
+            this.listBoxMatrix.Location = new System.Drawing.Point(435, 100);
+            this.listBoxMatrix.Name = "listBoxMatrix";
+            this.listBoxMatrix.Size = new System.Drawing.Size(183, 186);
+            this.listBoxMatrix.TabIndex = 6;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(731, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.firmaToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // firmaToolStripMenuItem
+            // 
+            this.firmaToolStripMenuItem.Name = "firmaToolStripMenuItem";
+            this.firmaToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.firmaToolStripMenuItem.Text = "Firma";
+            this.firmaToolStripMenuItem.Click += new System.EventHandler(this.firmaToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 453);
+            this.Controls.Add(this.listBoxMatrix);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonRead);
+            this.Controls.Add(this.buttonSize);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.textBoxM);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Arreglo TextBox Matriz";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBoxM;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.Button buttonSize;
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.ListBox listBoxMatrix;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem firmaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+    }
+}
+
---
+++ WFmatArregloTextbox/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFmatArregloTextbox
+{
+    public partial class Form1 : Form
+    {
+        int m, n;
+        TextBox[,] textBoxMatrix;
+        double[,] elem;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void firmaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("WFcruzlara_signature.exe");
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonRead_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    elem[i, j] = double.Parse(textBoxMatrix[i, j].Text);
+                }
+            }
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            string aux;
+
+            for (int i = 0; i < m; i++)
+            {
+                aux = " ";
+                for (int j = 0; j < n; j++)
+                {
+                    aux += elem[i, j] + " \t";
+                }
+
+                listBoxMatrix.Items.Add(aux);
+            }
+        }
+
+        private void buttonSize_Click(object sender, EventArgs e)
+        {
+            m = int.Parse(textBoxM.Text);
+            n = int.Parse(textBoxN.Text);
+            elem = new double[m, n];
+            textBoxMatrix = new TextBox[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    textBoxMatrix[i, j] = new TextBox();
+                    textBoxMatrix[i, j].Width = 30;
+                    textBoxMatrix[i, j].Height = 20;
+                    textBoxMatrix[i, j].Location = new Point(j * 30, i * 20);
+                    panel1.Controls.Add(textBoxMatrix[i, j]);
+                }
+            }
+        }
+    }
+}
